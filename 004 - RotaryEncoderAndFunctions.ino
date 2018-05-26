0f1cbc12de3f4546f26ee2aa163181ee6d8e61dd
Ryan L Dockstader <ryanldockstader@gmail.com>
2018-05-26 13:38:56 -0600

    Update 004 - RotaryEncoderAndFunctions.ino

--- 004 - RotaryEncoderAndFunctions.ino
+++ 004 - RotaryEncoderAndFunctions.ino
@@ -1,14 +1,14 @@
 // RGB LED
-const int redPin = 11;
-const int greenPin = 10;
-const int bluePin = 9;
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
 
-// Rotary Encoder
+// Rotary encoder
 const int PinCLK = 2;
 const int PinDT = 3;
 const int PinSW = 4;
 
-// Global Variables (we'll talk about this in another tutorial)
+// global variables (we'll talk about this in another tutorial)
 volatile boolean TurnDetected;
 volatile boolean up;
 long blueVal = 0;
@@ -16,9 +16,9 @@ long greenVal = 0;
 long redVal = 0;
 long virtualPosition = 0;
 long previousPosition = 0;
-int curPin = 9;
+int curPin = bluePin; //First pin is blue pin.
 
-// Functions
+// isr function
 void isr() {
   if(digitalRead(PinCLK))
     up = digitalRead(PinDT);
@@ -28,17 +28,18 @@ void isr() {
   TurnDetected = true;
 }
 
+
 // Declarations
+void cycleColors();
 long readRotaryEncoder();
 void setPinValue(int value, int pin);
 void displayChange();
-void autoCycleColors();
 
 void setup() {
   // Setup RGB LED
   pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
 
   // Setup Rotary Encoder
   pinMode(PinCLK, INPUT);
@@ -48,22 +49,21 @@ void setup() {
 
   // Start serial monitor
   Serial.begin(9600);
-  Serial.println("Serial Monitor Started...");
+  Serial.println("serial monitor started...");
 
 }
 
 void loop() {
+  //cycleColors(); We may bring this back later...
   virtualPosition += readRotaryEncoder();
-  if(!(digitalRead(PinSW))) { // check if push button is pressed
+  if(!(digitalRead(PinSW))) { // Check if push button is pressed
     setPin();
-    delay(500); // make sure we aren't double reading a switch...
+    delay(500);
   }
-
-
   setPinValue(virtualPosition, curPin);
   displayChange();
-
 }
+
 
 void setPin() {
   if(curPin == bluePin) {
@@ -71,18 +71,19 @@ void setPin() {
     blueVal = virtualPosition;
     virtualPosition = greenVal;
     Serial.println("Pin set to green...");
-    } else if (curPin == greenPin) {
-      curPin = redPin;
-      greenVal = virtualPosition;
-      virtualPosition = redVal;
-      Serial.println("Pin set to Red");
-    } else if (curPin == redPin) {
-      curPin = bluePin;
-      redVal = virtualPosition;
-      virtualPosition = blueVal;
-      Serial.println("Pin set to Blue");
-    }
+  } else if (curPin == greenPin) {
+    curPin = redPin;
+    greenVal = virtualPosition;
+    virtualPosition = redVal;
+    Serial.println("pin set to red...");
+  } else if (curPin == redPin) {
+    curPin = bluePin;
+    redVal = virtualPosition;
+    virtualPosition = blueVal;
+    Serial.println("pin set to blue...");
+  }
 }
+
 
 void setPinValue(int value, int pin) {
   if(value >= 0 && value <= 255) {
@@ -105,49 +106,44 @@ void displayChange() {
   }
 }
 
+
 long readRotaryEncoder() {
-  long virtualPosition = 0;
-  if (TurnDetected) {
-    if(up)
-      virtualPosition--; // Mine is backwards from the original example....
-    else
+  long virtualpos = 0;
+  if(TurnDetected) {
+    if(up) {
+      virtualPosition--; // My rotary encoder turns the oposite way of the form code...
+    } else {
       virtualPosition++;
-
-    TurnDetected = false;
+    }
   }
-
-  return virtualPosition;
 }
 
-void autoCycleColors() {
-  for(int i = 0; i < 256; i++)
-  {
+
+void cycleColors() {
+  // Increase the brightness
+  for(int i = 0; i < 256; i++) {
     analogWrite(redPin, i);
-    delay(10);
+    delay(10); //Added so you can see the lights adjusting
   }
-  for(int i = 0; i < 256; i++)
-  {
+  for(int i = 0; i < 256; i++) {
     analogWrite(bluePin, i);
-    delay(10);
+    delay(10); //Added so you can see the lights adjusting
   }
-  for(int i = 0; i < 256; i++)
-  {
+  for(int i = 0; i < 256; i++) {
     analogWrite(greenPin, i);
-    delay(10);
+    delay(10); //Added so you can see the lights adjusting
   }
-    for(int i = 256; i >= 0; i--)
-  {
+  // Decrease the brightness
+  for(int i = 255; i > 0; i--) {
     analogWrite(redPin, i);
-    delay(10);
+    delay(10); //Added so you can see the lights adjusting
   }
-      for(int i = 256; i >= 0; i--)
-  {
-    analogWrite(greenPin, i);
-    delay(10);
-  }
-      for(int i = 256; i >= 0; i--)
-  {
+  for(int i = 255; i > 0; i--) {
     analogWrite(bluePin, i);
-    delay(10);
-  }  
+    delay(10); //Added so you can see the lights adjusting
+  }
+  for(int i = 255; i > 0; i--) {
+    analogWrite(greenPin, i);
+    delay(10); //Added so you can see the lights adjusting
+  }
 }
